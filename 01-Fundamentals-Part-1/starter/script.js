216db96b66e566464019b6acad82192229928149
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-12-22 08:20:20 +0530

    data type

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -12,15 +12,30 @@ let firstName = "Aditya";
 let $ = "Jayantilal";
 let last_name = "Parmar";
 const PI = 3.1415;
-const _ = "BY"
+const _ = "BY";
 
-let _$ = "JS legal variable"
+let _$ = "JS legal variable";
 
 // cannot use reserved Keyword
 // const function = "This is a reserved Keyword"
 // Instead can do something like
 
-const $function = "This is legal"
-const _new = "Hello"
+const $function = "This is legal";
+const _new = "Hello";
 
 console.log(_$);
+
+let isIsland = false;
+
+console.log(typeof isIsland);
+console.log(typeof 25);
+console.log(typeof "JavaScript");
+
+isIsland = "No";
+console.log(typeof isIsland);
+
+let year;
+console.log(typeof year);
+
+// will show the object as type for null which is a bug/error in javascript since long
+console.log(typeof null);

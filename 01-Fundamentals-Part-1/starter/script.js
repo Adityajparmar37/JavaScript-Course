0271a4d48a1b483e1108b7e48a7eb7f7e1116c10
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-12-22 07:56:29 +0530

    JS fundamental Part - 1 lec 10

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+let js = "JavaScript";
+
+if (js === "JavaScript") alert("Hello from JS");
+console.log(20 + 50);
+
+let country = "India";
+let continent = "Asia";
+let population = 56;
+
+//variable name conventions
+let firstName = "Aditya";
+let $ = "Jayantilal";
+let last_name = "Parmar";
+const PI = 3.1415;
+const _ = "BY"
+
+let _$ = "JS legal variable"
+
+// cannot use reserved Keyword
+// const function = "This is a reserved Keyword"
+// Instead can do something like
+
+const $function = "This is legal"
+const _new = "Hello"
+
+console.log(_$);

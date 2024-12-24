5ea40aaf256f714ba9658d3f4e5b6090f28a3871
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-12-24 23:27:12 +0530

    JS Fun part - 2 , ep - funtions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,10 +9,35 @@ if (hasDriversLicense) console.log("I can drive :D");
 // const interface = "Audio";
 // const private = 534;
 
+// this is function declaration
+function cutFruits(fruit) {
+  return fruit * 4;
+}
+
 function fruitProcessor(apply, orange) {
-  const juice = `Juice of ${apply} apply and ${orange} orange`;
+  const applyPieces = cutFruits(apply);
+  const orangePieces = cutFruits(orange);
+  const juice = `Juice of ${applyPieces} apply and ${orangePieces} orange`;
   return juice;
 }
 
 const JuiceReady = fruitProcessor(4, 5);
 console.log(JuiceReady);
+
+// this is function expression
+const fruitJuice = function (orange, lemon) {
+  return `Juice of ${orange} orange and ${lemon} lemon`;
+};
+
+console.log(fruitJuice(5, 2));
+
+//arrow function
+const calAge = (birthYear) => 2025 - birthYear;
+
+const yearUntilRetirement = (birthYear, name) => {
+  const age = 2025 - birthYear;
+  const retirement = 65 - age;
+  return `${retirement} age ${name} will be retired`;
+};
+
+console.log(yearUntilRetirement(2003, "Aditya"));
